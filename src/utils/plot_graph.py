30348bd992b9085e6ec7b683f5a8faa67093015c
Antoine_Venturini <a.venturini10@studenti.unipi.it>
2024-05-16 15:35:14 +0200

    Plot pTC nodes

--- src/utils/plot_graph.py
+++ src/utils/plot_graph.py
@@ -4,17 +4,25 @@ Plotting the 2D graph of hits based on the 'edgeMatrix.txt' file
 import numpy as np
 import matplotlib.pyplot as plt
 
-def calculate_coordinates(node_id):
-    # Evaluate x-y coordinate of a wire at CDCH center:
+
+def calculate_coordinates(node_id, pixel_geo):
+    # Evaluate x-y coordinate of a wire and TC tile at CDCH center:
     # input:
-    # node_id = wire ID
-    layer = int((node_id - 192) / 192)
-    wireID_in_layer = node_id % 192
-    radius = 270 - layer * 10;
-    angle = wireID_in_layer * 360 / 192 * (np.pi / 180);
-    x = radius * np.cos(-(angle) + 30. * np.pi / 180.)
-    y = radius * np.sin(-(angle) + 30. * np.pi / 180.)
-    return x, y
+    # node_id = wire ID (if < 1920) or pixelID (if > 1920)
+    if node_id < 1920:
+        layer = int((node_id - 192) / 192)
+        wireID_in_layer = node_id % 192
+        radius = 270 - layer * 10;
+        angle = wireID_in_layer * 360 / 192 * (np.pi / 180);
+        x = radius * np.cos(-(angle) + 30. * np.pi / 180.)
+        y = radius * np.sin(-(angle) + 30. * np.pi / 180.)
+        hittype = 0
+    else:
+        pixel_id = node_id - 1920
+        x = pixel_geo[pixel_id][1]
+        y = pixel_geo[pixel_id][2]
+        hittype = 1
+    return hittype, x, y
 
 def read_adjacency_matrix(file_path):
     # Read adjacency matrix from file
@@ -28,6 +36,14 @@ def plot_graph(adjacency_matrix):
     is_node_plotted = np.zeros(num_nodes, dtype='int32')
     print(f"Number of possible nodes = {num_nodes}")
 
+    number_of_nodes = 0
+    number_of_edges = 0
+
+    pixel_geo = np.loadtxt("spxGeometry.txt")
+
+    colors = ["pink", "blue"] # colors for CDCH and TC nodes
+    fmts = ["o", "s"] # fmts for CDCH and TC nodes
+
     # Plot nodes and edges 
     for node_id in range(num_nodes):
         # Put a cut on connections contributing less then 0.1% in the number of connections
@@ -40,19 +56,24 @@ def plot_graph(adjacency_matrix):
 
                 # Plot nodes
                 if not is_node_plotted[node_id]:
-                    node_x, node_y = calculate_coordinates(node_id)
-                    plt.errorbar(node_x, node_y, fmt='o', alpha=.5, markersize=10, color='blue')
+                    hittype, node_x, node_y = calculate_coordinates(node_id, pixel_geo)
+                    plt.errorbar(node_x, node_y, fmt=fmts[hittype], alpha=.5, markersize=10, color=colors[hittype])
                     is_node_plotted[node_id] = 1
+                    number_of_nodes += 1
                 if not is_node_plotted[j]:
-                    node_x, node_y = calculate_coordinates(j)
-                    plt.errorbar(node_x, node_y, fmt='o', alpha=.5, markersize=10, color='blue')
+                    hittype, node_x, node_y = calculate_coordinates(j, pixel_geo)
+                    plt.errorbar(node_x, node_y, fmt=fmts[hitttype], alpha=.5, markersize=10, color=colors[hittype])
                     is_node_plotted[j] = 1
-
+                    number_of_nodes += 1
                 # Plot edges
                 if adjacency_matrix[node_id][j] > cut:
-                    xi, yi = calculate_coordinates(node_id)
-                    xj, yj = calculate_coordinates(j)
+                    hittype, xi, yi = calculate_coordinates(node_id, pixel_geo)
+                    hittype, xj, yj = calculate_coordinates(j, pixel_geo)
                     plt.plot([xi, xj], [yi, yj], 'k-', linewidth=0.5, alpha=.5)
+                    number_of_edges += 1
+
+    print(f"Number of nodes = {number_of_nodes}")
+    print(f"Number of edges = {number_of_edges}")
 
     plt.axis('off')
     plt.axis('equal')
