ff66826f14c303bbfa567f865cb3aefc2a37b959
Calisse <lapodispoto@gmail.com>
2025-05-01 21:38:40 +0200

    typos

--- src/CompareTestAndTruth.py
+++ src/CompareTestAndTruth.py
@@ -3,7 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.cm import tab10
 from utils.tools import load_graph_npz
-from utils.plot_graph import plot
+
+
+"""
+Careful! Import plot either for utils.plot_graph or from utils.plot_graph_node_classification depending on task, this is temporary, will be solved.
+
+
+"""
+
+#from utils.plot_graph import plot
+from utils.plot_graph_node_classification import plot
 import pandas as pd
 import sys
 from models.interaction_network import InteractionNetwork
@@ -12,8 +21,7 @@ from utils.dataset import GraphDataset
 def plot_example(filename):
     graph = np.load(filename)
     
-    print(graph['X'].shape)
-    print(graph['edge_index'].shape)
+   
     edge_index =graph['edge_index']
 
     startindex = np.min([np.min(edge_index[0]), np.min(edge_index[1])]) 
@@ -21,8 +29,6 @@ def plot_example(filename):
     edge_index[0] += -startindex
     edge_index[1] += -startindex
 
-    print(graph['truth'].shape)
-    print(graph['predicted'].shape)
 
     plot(graph['X'],edge_index, graph['truth'])
     
@@ -47,7 +53,7 @@ if __name__ == "__main__":
     
     
     
-    filename = fz"DataTruthPredicted/file01030_event{int(sys.argv[1])}_sectors0.npz_test_pred_truth.npz"
+    filename = f"TruthWithNoise/{int(sys.argv[1])}_test_pred_truth.npz"
     
     
     plot_example(filename)
